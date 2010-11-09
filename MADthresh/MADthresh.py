d77c7a7b1b3a92ddc51f4b0d675ac31809c6f4a3
Thouis (Ray) Jones <thouis@gmail.com>
2010-11-09 01:09:07 +0100

    Initial MADthresh work.
    Odd oscillations in error metric.  Are they real?
    more testing needed.  For a few test probes, the
    MAD calculating is accurate.
    
    TODO
    - subsampling
    - illum correction by low-decile filtering
    - hierarchical thresholding
    - adaptive thresh (does this remove need for 
      illum correction?)

--- MADthresh/MADthresh.py
+++ MADthresh/MADthresh.py
@@ -7,18 +7,56 @@ matplotlib.use('WxAgg')
 import pylab
 import Image
 from cpmath.outline import outline
+from cpmath.filter import median_filter
 
-def MAD_based_error(im):
-    hist, bins = np.histogram(im, 100)
-    # bins is the set of possible thresholds.
     
-    # compute medians of the lower intensity of pixels for every possible threshold
     
-
 def wMADthresh(im):
-    err = MAD_based_error(im)
-
+    # Flatten the image, and sort it
+    vals = np.sort(im.copy().flatten())
+    # Every value is a potential threshold.
+    # Get an index to each value.
+    idx = np.arange(vals.size, dtype=np.int)
+    # The medians of the values below and above the threshold are
+    # halfway between that value and the end of the array.
+    lo_median = (vals[idx / 2] + vals[(idx + 1) / 2]) / 2.0
+    hi_median = vals[vals.size - (vals.size - idx) / 2 - 1]
+    # The MAD of an array X of length N can be found by
+    # finding two indices, L and H, where:
+    #    H - L = N / 2, and
+    #    X[H] - median(X) = median(X) - X[L]
+    # To find the MAD for each possible threshold, for the low and
+    # high fractions, we'll use parallel binary search.
     
+    # H/L for low set
+    lo_L = np.zeros(vals.size, np.int)
+    lo_H = lo_L + idx / 2 # first property, and invariant in the loop below
+    
+    # H/L for high set
+    hi_L = np.zeros(vals.size, np.int)
+    hi_H = idx[::-1] / 2
+
+    stepsize = 2 ** int(np.log2(vals.size))
+    while stepsize > 0:
+        # sign indicates which direction to move lo_L and hi_L
+        step = stepsize * np.sign((lo_median - vals[lo_L]) - (vals[lo_H] - lo_median)).astype(int)
+        lo_L += step
+        np.clip(lo_L, 0, vals.size - 1 - idx / 2, lo_L)
+        lo_H = lo_L + idx / 2
+
+        step = stepsize * np.sign((hi_median - vals[hi_L]) - (vals[hi_H] - hi_median)).astype(int)
+        hi_L += step
+        np.clip(hi_L, 0, vals.size - 1 - idx[::-1] / 2, hi_L)
+        hi_H = hi_L + idx[::-1] / 2
+        stepsize /= 2
+
+    err = idx * (lo_median - vals[lo_L]) + idx[::-1] * (hi_median - vals[hi_L])
+    loerr = (lo_median - vals[lo_L])
+    loerr = loerr[2:] + loerr[:-2]
+    pylab.plot(loerr)
+    pylab.show()
+    err = err[2:] + err[:-2]
+    return vals[err == err.min()].mean()
     
 
 class ImagePanel(wx.lib.scrolledpanel.ScrolledPanel):
@@ -43,7 +81,7 @@ class ImagePanel(wx.lib.scrolledpanel.ScrolledPanel):
 
     def threshold(self, thresh):
         print thresh
-        return self.im + im.max() * outline(self.im > thresh)
+        return self.im + im.max() * outline(self.im > thresh) / 10.0
 
     def im_to_bmp(self, im):
         h, w = im.shape
@@ -104,6 +142,17 @@ im = PIL_to_numpy(Image.open(sys.argv[1]))
 im = np.log(im.astype(float))
 im -= im.min()
 im /= im.max()
+
+im -= median_filter(im, im > -1, 55)
+im -= im.min()
+im /= im.max()
+
+
+pylab.hist(im.flatten(), 100)
+pylab.show()
+
+
+print wMADthresh(im)
 
 app = wx.App(0)
 
