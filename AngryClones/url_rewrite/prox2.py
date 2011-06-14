892a376bcf8fd123a5c8ffeffc0731bb5aba8f4e
Thouis (Ray) Jones <thouis@gmail.com>
2011-06-14 22:10:33 +0200

    add cache/replace directories

--- AngryClones/url_rewrite/prox2.py
+++ AngryClones/url_rewrite/prox2.py
@@ -3,38 +3,66 @@ from twisted.web import http
 from twisted.web.proxy import Proxy, ProxyRequest, ProxyClientFactory, ProxyClient
 from ImageFile import Parser
 from StringIO import StringIO
+import threading
+import os.path
+import urllib
+
+cachedir = 'cache'
+replacedir = 'replace'
+
+def cache_file(uri):
+    if not 'chrome.angrybirds.com' in uri:
+        return None
+    return os.path.join(cachedir, uri.split('chrome.angrybirds.com')[-1].replace('/', '_'))
+
+def replace_file(uri):
+    if not 'chrome.angrybirds.com' in uri:
+        return None
+    replacement = os.path.join(replacedir, uri.split('chrome.angrybirds.com')[-1].replace('/', '_'))
+    if os.path.exists(replacement):
+        return replacement
+    return None
 
 class InterceptingProxyClient(ProxyClient):
     def __init__(self, *args, **kwargs):
         ProxyClient.__init__(self, *args, **kwargs)
-        self.image_replacing = None
-        if 'INGAME_BIRDS_PIGS.png' in self.father.uri:
-            print "REPLACING"
-            self.image_replacing = True
+        self.replacing = None
+        cache_path = cache_file(self.father.uri)
+        replace_path = replace_file(self.father.uri)
+        if cache_path and not os.path.exists(cache_path):
+            print "FETCHING", cache_path
+            t = threading.Thread(target=lambda:urllib.urlretrieve(self.father.uri, cache_path))
+            t.start()
+        if replace_path:
+            print "replacing %s with %s" % (self.father.uri, replace_path)
+            self.replacing = replace_path
 
     def handleHeader(self, key, value):
-        if key == "Content-Length" and self.image_replacing:
+        if key == "Content-Length" and self.replacing is not None:
+            pass
+        if key == "Content-Encoding" and value == 'gzip' and self.replacing is not None:
             pass
         else:
             ProxyClient.handleHeader(self, key, value)
 
     def handleEndHeaders(self):
-        if self.image_replacing:
+        if self.replacing is not None:
             pass  # Need to calculate and send Content-Length first
         else:
             ProxyClient.handleEndHeaders(self)
 
     def handleResponsePart(self, buffer):
-        if self.image_replacing:
+        if self.replacing is not None:
             pass
         else:
             ProxyClient.handleResponsePart(self, buffer)
 
     def handleResponseEnd(self):
-        if self.image_replacing:
+        if self.replacing is not None:
             try:
-                buffer = open("/Users/thouis/INGAME_BIRDS_PIGS.png").read()
+                buffer = open(self.replacing).read()
             except:
+                print "FAIL", self.replacing
                 buffer = ""
             ProxyClient.handleHeader(self, "Content-Length", len(buffer))
             ProxyClient.handleEndHeaders(self)
