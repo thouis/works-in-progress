9319c81fbede92c05516f33b44329f244a054423
Thouis (Ray) Jones <thouis@gmail.com>
2011-07-04 12:00:24 +0200

    this version works

--- curie_to_google/forward.py
+++ curie_to_google/forward.py
@@ -1,6 +1,7 @@
 import getpass
 import imaplib
 import sys
+import time
 
 def cpimap(user1, host1, pass1, user2, host2, pass2):
     m1 = imaplib.IMAP4_SSL(host1, 993)
@@ -8,18 +9,14 @@ def cpimap(user1, host1, pass1, user2, host2, pass2):
     m1.login(user1, pass1)
     m2.login(user2, pass2)
 
-    print 'Copying', f
     m1.select('INBOX')
-    m2.select('Inbox')
-    print 'Fetching messages...'
-    typ, data = m1.search(None, 'UNREAD')
+    typ, data = m1.search(None, 'UNSEEN')
     msgs = data[0].split()
-    sys.stdout.write(" ".join(['Copying', str(len(msgs)), 'messages']))
-    for num in msgs:
+    print 'Copying', str(len(msgs)), 'messages'
+    for ct, num in enumerate(msgs):
         typ, data = m1.fetch(num, '(RFC822)')
-        sys.stdout.write('%s' % num)
-        m2.append(f, None, None, data[0][1])
-        sys.stdout.write('\n')
+        print ct + 1
+        m2.append('Inbox', None, None, data[0][1])
 
 h1 = 'mail.curie.fr'
 u1 = 'tjones'
@@ -28,4 +25,6 @@ h2 = 'imap.gmail.com'
 u2 = 'thouis'
 pass2 = getpass.getpass('PW google: ')
 
-cpimap(u1, h2, pass1, u2, h2, pass2)
+while True:
+    cpimap(u1, h1, pass1, u2, h2, pass2)
+    time.sleep(300)
